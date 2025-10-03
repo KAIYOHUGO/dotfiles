8a176d7139547cc412d6220d2c04c8f87fcaac70
kaiyohugo <41114603+KAIYOHUGO@users.noreply.github.com>
2025-10-03 16:59:39 +0800

    fix: fix keymap

--- hosts/cx5500/keymap.nix
+++ hosts/cx5500/keymap.nix
@@ -15,23 +15,38 @@
         )
 
         (defsrc
-          lmeta bck fwd fsc scl sys brdown brup mute vold volu sleep min
+          esc  bck  fwd  fsc  scl  sys  brdn brup mute vold volu zzz
+          grv  1    2    3    4    5    6    7    8    9    0    -    =    bspc
+          tab  q    w    e    r    t    y    u    i    o    p    [    ]    \
+          caps a    s    d    f    g    h    j    k    l    ;    '    ret
+          lsft z    x    c    v    b    n    m    ,    .    /    rsft
+          lctl lmet lalt           spc            ralt rmet rctl
         )
 
         (deflayer default
-          @lmeta f1 f2 f3 f4 f5 f6 f7 f8 f9 f10 f11 min
+          esc  f1   f2   f3   f4   f5   f6   f7   f8   f9   f10  f11
+          grv  1    2    3    4    5    6    7    8    9    0    -    =    bspc
+          tab  q    w    e    r    t    y    u    i    o    p    [    ]    \
+          caps a    s    d    f    g    h    j    k    l    ;    '    ret
+          lsft z    x    c    v    b    n    m    ,    .    /    rsft
+          lctl @met lalt           spc            ralt rmet rctl
         )
 
         (deflayer func
-          _ bck fwd fsc scl M-sys M-brdown M-brup M-mute M-vold M-volu sleep f12
+          esc  bck  fwd  fsc  scl  sys  brdn brup mute vold volu zzz
+          grv  M-1  M-2  M-3  M-4  M-5  M-6  M-7  M-8  M-9  M-0  f12  =    bspc
+          tab  M-q  M-w  M-e  M-r  M-t  M-y  M-u  M-i  M-o  M-p  M-[  M-]    \
+          caps M-a  M-s  M-d  M-f  M-g  M-h  M-j  M-k  M-l  M-;  M-'  ret
+          lsft M-z  M-x  M-c  M-v  M-b  M-n  M-m  M-,  M-.  M-/  rsft
+          lctl lmet lalt           spc            ralt rmet rctl
         )
 
-
-        (defalias lmeta (multi (layer-while-held func) rmeta))
+        (defalias met (layer-while-held func))
       '';
 
       extraDefCfg = ''
         linux-dev /dev/input/by-path/platform-i8042-serio-0-event-kbd
+        process-unmapped-keys yes
       '';
     };
   };    
