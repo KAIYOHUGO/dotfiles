7ee259e3118aacc014d85a048bdd6db26e9f75ee
kaiyohugo <41114603+KAIYOHUGO@users.noreply.github.com>
2025-10-08 23:21:17 +0800

    feat: cahnge cx5500 keymap from layer to override

--- hosts/cx5500/keymap.nix
+++ hosts/cx5500/keymap.nix
@@ -11,37 +11,36 @@
           fwd 173
           fsc 372
           scl 120
-
         )
+        (defsrc)
+        (deflayer default)
+        (defoverrides
+          (bck)  (f1)
+          (fwd)  (f2)
+          (fsc)  (f3)
+          (scl)  (f4)
+          (sys)  (f5)
+          (brdn) (f6)
+          (brup) (f7)
+          (mute) (f8)
+          (vold) (f9)
+          (volu) (f10)
+          (zzz)  (f11)
 
-        (defsrc
-          esc  bck  fwd  fsc  scl  sys  brdn brup mute vold volu zzz
-          grv  1    2    3    4    5    6    7    8    9    0    -    =    bspc
-          tab  q    w    e    r    t    y    u    i    o    p    [    ]    \
-          caps a    s    d    f    g    h    j    k    l    ;    '    ret
-          lsft z    x    c    v    b    n    m    ,    .    /    rsft
-          lctl lmet lalt           spc            ralt rmet rctl
+          ;; layer
+          (lmet bck)  (bck)
+          (lmet fwd)  (fwd)
+          (lmet fsc)  (fsc)
+          (lmet scl)  (scl)
+          (lmet sys)  (sys)
+          (lmet brdn) (brdn)
+          (lmet brup) (brup)
+          (lmet mute) (mute)
+          (lmet vold) (vold)
+          (lmet volu) (volu)
+          (lmet zzz)  (zzz)
+          (lmet -)    (f12)
         )
-
-        (deflayer default
-          esc  f1   f2   f3   f4   f5   f6   f7   f8   f9   f10  f11
-          grv  1    2    3    4    5    6    7    8    9    0    -    =    bspc
-          tab  q    w    e    r    t    y    u    i    o    p    [    ]    \
-          caps a    s    d    f    g    h    j    k    l    ;    '    ret
-          lsft z    x    c    v    b    n    m    ,    .    /    rsft
-          lctl @met lalt           spc            ralt rmet rctl
-        )
-
-        (deflayer func
-          esc  bck  fwd  fsc  scl  sys  brdn brup mute vold volu zzz
-          grv  M-1  M-2  M-3  M-4  M-5  M-6  M-7  M-8  M-9  M-0  f12  =    bspc
-          tab  M-q  M-w  M-e  M-r  M-t  M-y  M-u  M-i  M-o  M-p  M-[  M-]    \
-          caps M-a  M-s  M-d  M-f  M-g  M-h  M-j  M-k  M-l  M-;  M-'  ret
-          lsft M-z  M-x  M-c  M-v  M-b  M-n  M-m  M-,  M-.  M-/  rsft
-          lctl lmet lalt           spc            ralt rmet rctl
-        )
-
-        (defalias met (layer-while-held func))
       '';
 
       extraDefCfg = ''
