89e084fd22dea5aa012444abc9b45a025887f339
kaiyohugo <41114603+KAIYOHUGO@users.noreply.github.com>
2025-10-20 10:38:48 +0800

    feat: add nix-ld

--- nixos/patch/default.nix
+++ nixos/patch/default.nix
@@ -11,9 +11,13 @@ let
   libinput-quirk = import ./libinput-quirk.nix {
     inherit config;
   };
+  nix-ld = import ./nix-ld.nix {
+    inherit config pkgs;
+  };
 
 in
 merge [
   pop-icon-key
   libinput-quirk
+  nix-ld
 ]
